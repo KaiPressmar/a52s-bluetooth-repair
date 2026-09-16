e73b159d41b602d9e66c1ef7b871268780a921fd
kpre <Kai.Pressmar@gmail.com>
2026-09-16 20:57:11 +0200

    Configure Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "de.kaipressmar.a52srepair"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "de.kaipressmar.a52srepair"
+        minSdk = 31
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.3.0"
+    }
+}
